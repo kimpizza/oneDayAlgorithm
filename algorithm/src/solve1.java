fd8ae3d1870b30a8c5120d6a3a44f23cfef58d68
kimpizza <dsuis1310009@naaver.com>
2024-03-20 02:13:26 +0900

    solved_1day

--- algorithm/src/solve1.java
+++ algorithm/src/solve1.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 public class solve1 {
     public static void main(String[] args) {
-        System.out.printf("ddd");
+        int [] num_list = {3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1};
+
+        System.out.println(solution(num_list));
+    }
+    public static int solution(int[] num_list){
+        int answer = 0;
+
+        if(num_list.length >= 11) {
+            for(int i = 0; i<num_list.length; i++) {
+                answer += num_list[i];
+            }
+        } else{
+            answer = 1;
+            for(int i=0; i<num_list.length; i++) {
+                answer *= num_list[i];
+            }
+        }
+        return answer;
     }
 }
